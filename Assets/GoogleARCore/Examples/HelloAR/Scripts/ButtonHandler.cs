b1f4b3c77d3becf60243c5c13ab7d043697cd55a
Kartikey Kumar <kartikey.zen@gmail.com>
2023-02-25 20:22:00 +0530

    init

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ButtonHandler.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using GoogleARCore.Examples.HelloAR;
+
+public class ButtonHandler : MonoBehaviour
+{
+    GameObject controllerGO;
+    HelloARController scriptMyHelloAR;
+    GameObject sphereGO;
+    GameObject arrowGO;
+    GameObject wrongWayGO;
+    GameObject thisWay1;
+    GameObject thisWay2;
+    GameObject destinationGO;
+    public void ChangeObjectTypeToDestination()
+    {
+        controllerGO = GameObject.Find("HelloARController");
+        scriptMyHelloAR = controllerGO.GetComponent<HelloARController>();
+        destinationGO = GameObject.Find("destination");
+        scriptMyHelloAR.GameObjectHorizontalPlanePrefab = destinationGO;
+        scriptMyHelloAR.GameObjectPointPrefab = destinationGO;
+    }
+    public void ChangeObjectTypeToWrongWay()
+    {
+        controllerGO = GameObject.Find("HelloARController");
+        scriptMyHelloAR = controllerGO.GetComponent<HelloARController>();
+        wrongWayGO = GameObject.Find("wrong_way");
+        scriptMyHelloAR.GameObjectHorizontalPlanePrefab = wrongWayGO;
+        scriptMyHelloAR.GameObjectPointPrefab = wrongWayGO;
+    }
+    public void ChangeObjectTypeToThisWay1()
+    {
+        controllerGO = GameObject.Find("HelloARController");
+        scriptMyHelloAR = controllerGO.GetComponent<HelloARController>();
+        thisWay1 = GameObject.Find("this_way_1");
+        scriptMyHelloAR.GameObjectHorizontalPlanePrefab = thisWay1;
+        scriptMyHelloAR.GameObjectPointPrefab = thisWay1;
+    }
+    public void ChangeObjectTypeToThisWay2()
+    {
+        controllerGO = GameObject.Find("HelloARController");
+        scriptMyHelloAR = controllerGO.GetComponent<HelloARController>();
+        thisWay2 = GameObject.Find("this_way_2");
+        scriptMyHelloAR.GameObjectHorizontalPlanePrefab = thisWay2;
+        scriptMyHelloAR.GameObjectPointPrefab = thisWay2;
+    }
+    public void ChangeObjectTypeToSphere()
+    {
+        controllerGO = GameObject.Find("HelloARController");
+        scriptMyHelloAR = controllerGO.GetComponent<HelloARController>();
+        sphereGO = GameObject.Find("sphere");
+        scriptMyHelloAR.GameObjectHorizontalPlanePrefab = sphereGO;
+        scriptMyHelloAR.GameObjectPointPrefab = sphereGO;
+    }
+    public void ChangeObjectTypeToArrow()
+    {
+        controllerGO = GameObject.Find("HelloARController");
+        scriptMyHelloAR = controllerGO.GetComponent<HelloARController>();
+        arrowGO = GameObject.Find("arrow");
+        scriptMyHelloAR.GameObjectHorizontalPlanePrefab = arrowGO;
+        scriptMyHelloAR.GameObjectPointPrefab = arrowGO;
+    }
+}
---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ToggleHandler.cs
@@ -0,0 +1,52 @@
+﻿using GoogleARCore.Examples.Common;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleHandler : MonoBehaviour
+{
+    bool showPlanes;
+    bool showButtons;
+    bool showFlag = true;
+    public int counter = 0;
+
+    public void OnToggle(bool flag)
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            flag = false;
+        } else
+        {
+            flag = true;
+        }
+        showFlag = !showFlag;
+        OnTogglePlanes(flag);
+        OnToggleButtons(flag);
+    }
+    public void OnTogglePlanes(bool flag)
+    {
+        showPlanes = flag;
+        foreach (GameObject plane in GameObject.FindGameObjectsWithTag("plane"))
+        {
+            Renderer r = plane.GetComponent<Renderer>();
+            DetectedPlaneVisualizer t = plane.GetComponent<DetectedPlaneVisualizer>();
+            r.enabled = flag;
+            t.enabled = flag;
+        }
+    }
+
+    public void OnToggleButtons(bool flag)
+    {
+        showButtons = flag;
+        foreach (GameObject button in GameObject.FindGameObjectsWithTag("hideableButton"))
+        {
+            Button btn = button.GetComponent<Button>();
+            btn.interactable = flag;
+            // btn.image.color = new Color(255f, 0f, 0f, .5f);
+            btn.gameObject.SetActive(flag);
+            button.SetActive(flag);
+        }
+    }
+}
